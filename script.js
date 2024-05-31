d7c3d28ef46c2b273747170aa6d95a13b556631a
Onyekwere Michael <onyekweremichael55@gmail.com>
2024-05-31 22:25:55 +0100

    weather

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+
 const apiKey = "d347d35b6f3ecab8e5037ad29b23872c";
 
 const inputElement = document.getElementById("js-input")
 const form = document.querySelector("form")
 const weather_data = document.getElementById('content')
+const weather_details = document.getElementById('details')
 
 form.addEventListener('submit', (event) => {
  event.preventDefault()
@@ -25,18 +27,17 @@ async function getWeather(inputValue) {
 
        const temp = Math.round(data.main.temp)
        const description = data.weather[0].description
-       const icon = data.weather[0].icon
-       const D_feels = Math.round(data.main.feels_like)
+       const image = data.weather[0].icon
+       console.log(`<img src="http://openweathermap.org/img/wn/${image}.png" alt="">`
+    )
        const humidity = data.main.humidity
        const wind_speed = data.wind.speed;
 
-       weather_data.querySelector(".ico").innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="">`
-
+       weather_data.querySelector('.ico').innerHTML =`<img src="http://openweathermap.org/img/wn/${image}.png" alt="weather icon"/>`
        weather_data.querySelector(".temp").textContent = `${temp}°c`
        weather_data.querySelector(".clouds").textContent = `${description}`
-       weather_data.querySelector(".details .Feels").textContent = `Feels like ${D_feels}`
        weather_data.querySelector(".details .Humidity").textContent = `Humidity ${humidity}`
-       weather_data.querySelector(".details .w-speed").textContent = `Wind speed ${wind_speed}`
+       weather_data.querySelector(" .details .w-speed").textContent = `Wind speed ${wind_speed}km/h`
 
     } catch (error) {
         
